36a0328b9a1976c827eb6da092241b5f21c050a5
Olaf Conradi <olaf@conradi.org>
2022-12-11 18:00:16 +0100

    AoC 2022 Day 4 Star 1

--- 2022/day04/run.py
+++ 2022/day04/run.py
@@ -5,10 +5,32 @@ from pathlib import Path
 
 def parse_data(input):
     """Parse input data."""
+    res = [[[int(x) for x in pair.split("-")] for pair in line.split(",")] for line in input.split("\n")]
+    return res
+
+
+def contained(pair):
+    (x1, y1) = pair[0]
+    (x2, y2) = pair[1]
+
+    if x1 > y1:
+        return False
+    if x2 > y2:
+        return False
+
+    if x1 == x2 or y1 == y2:
+        return True
+    if x1 < x2 and y2 <= y1:
+        return True
+    if x1 > x2 and y1 <= y2:
+        return True
+    return False
 
 
 def star1(data):
     """Solve puzzle for star 1."""
+    result = list(filter(contained, data))
+    return len(result)
 
 
 def star2(data):
