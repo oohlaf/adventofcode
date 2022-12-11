419d35868cd22aa120c8362bc68b17f298bcd7e7
Olaf Conradi <olaf@conradi.org>
2022-12-11 02:41:43 +0100

    AoC 2022 Day 2 Star 1

--- 2022/day02/run.py
+++ 2022/day02/run.py
@@ -1,14 +1,53 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
 from pathlib import Path
+
+
+MOVE_DICT = {
+    # Opponent moves
+    "A": np.array([1, 0, 0]),  # Rock
+    "B": np.array([0, 1, 0]),  # Paper
+    "C": np.array([0, 0, 1]),  # Scissors
+
+    # My moves
+    "X": np.array([1, 0, 0]),  # Rock
+    "Y": np.array([0, 1, 0]),  # Paper
+    "Z": np.array([0, 0, 1]),  # Scissors
+}
+
+# Opp x Me
+# 0 = loss, 3 = tie, 6 = win
+OUTCOME_SCORES = np.array([[3, 6, 0], [0, 3, 6], [6, 0, 3]])
+
+# X = 1, Y = 2, Z = 3
+SHAPE_SCORES = np.array([1, 2, 3])
 
 
 def parse_data(input):
     """Parse input data."""
+    game_play = [(line[0], line[2]) for line in input.split("\n")]
+    return game_play
+
+
+def game(move):
+    opp, me = move
+    opp_move = MOVE_DICT[opp]
+    my_move = MOVE_DICT[me]
+    game_move = np.outer(opp_move, my_move)
+    outcome_score = np.multiply(game_move, OUTCOME_SCORES)
+    shape_score = np.multiply(my_move, SHAPE_SCORES)
+    score = np.sum(outcome_score) + np.sum(shape_score)
+    return score
 
 
 def star1(data):
     """Solve puzzle for star 1."""
+    score = 0
+    for round in data:
+        score += game(round)
+    return score
 
 
 def star2(data):
@@ -22,7 +61,7 @@ def solve(input):
 
 
 def main():
-    input_file = Path(__file__).parent / "test_input.txt"
+    input_file = Path(__file__).parent / "input.txt"
     input_text = input_file.read_text().rstrip()
     solutions = solve(input_text)
     print("\n".join(str(s) for s in solutions))
