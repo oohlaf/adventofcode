5718e33280e3eb4d653816b6ea4c429fe991afeb
Olaf Conradi <olaf@conradi.org>
2022-12-11 23:41:32 +0100

    AoC 2022 Day 5 Star 1

--- 2022/day05/run.py
+++ 2022/day05/run.py
@@ -5,10 +5,53 @@ from pathlib import Path
 
 def parse_data(input):
     """Parse input data."""
+    result = [line for line in input.split("\n\n")]
+    drawing = list(result[0].split("\n"))
+    actions = list(result[1].split("\n"))
+    drawing.reverse()
+    return (drawing, actions)
+
+
+def build_state(drawing):
+    # Initialize each stack with correspinding label
+    header = [int(x) for x in drawing[0].split(" ") if x != ""]
+    # print(header)
+    state = {}
+    for i in header:
+        state[i] = []
+    # Fill each stack
+    for line in drawing[1:]:
+        for i, pos in zip(range(1, len(header) + 1), range(1, 4 * len(header), 4)):
+            # print(f"{i}:{pos} = {line[pos]}")
+            if line[pos] != " ":
+                state[i].append(line[pos])
+    return state
+
+
+def operate_crane(state, actions):
+    for action in actions:
+        # Move n from stack1 to stack2
+        _, num, _, stack1, _, stack2 = action.split(" ")
+        num = int(num)
+        stack1 = int(stack1)
+        stack2 = int(stack2)
+        items = [state[stack1].pop() for _ in range(num)]
+        state[stack2].extend(items)
+    return state
+
+
+def top_crate_message(state):
+    top = [state[stack].pop() for stack in state.keys()]
+    message = "".join(top)
+    return message
 
 
 def star1(data):
     """Solve puzzle for star 1."""
+    drawing, actions = data
+    state = build_state(drawing)
+    state = operate_crane(state, actions)
+    return top_crate_message(state)
 
 
 def star2(data):
