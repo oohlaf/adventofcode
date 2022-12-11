a9a982b5db11d73967520c7f25e4c8a43cdac005
Olaf Conradi <olaf@conradi.org>
2022-12-12 00:50:07 +0100

    AoC 2022 Day 6 Star 1

--- 2022/day06/run.py
+++ 2022/day06/run.py
@@ -5,10 +5,36 @@ from pathlib import Path
 
 def parse_data(input):
     """Parse input data."""
+    return [line for line in input.split("\n")]
+
+
+def is_marker(data):
+    return len(data) == len(set(data))
+
+
+def process(data):
+    buffer = []
+    i = 0
+    for x in data:
+        i += 1
+        if i > 4:
+            buffer.pop(0)
+        if i > 3:
+            buffer.append(x)
+            if is_marker(buffer):
+                return i
+        else:
+            buffer.append(x)
+    raise ValueError
 
 
 def star1(data):
     """Solve puzzle for star 1."""
+    line_positions = []
+    for line in data:
+        pos = process(line)
+        line_positions.append(pos)
+    return line_positions
 
 
 def star2(data):
