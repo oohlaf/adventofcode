eff9fb4785a5d26e3b2ab748b91534e78ca815db
Olaf Conradi <olaf@conradi.org>
2022-12-11 12:41:34 +0100

    AoC 2022 Day 3 Start 1

--- 2022/day03/run.py
+++ 2022/day03/run.py
@@ -1,14 +1,48 @@
 #!/usr/bin/env python3
 
+import string
+
 from pathlib import Path
+
+
+def build_prio_dict():
+    prio_dict = {}
+    for i in range(len(string.ascii_letters)):
+        prio_dict[string.ascii_letters[i]] = i + 1
+    return prio_dict
+
+
+def process_input(input):
+    for line in input.split("\n"):
+        if len(line) % 2:
+            raise ValueError
+        middle = len(line) // 2
+        yield (line[:middle], line[middle:])
 
 
 def parse_data(input):
     """Parse input data."""
+    return list(process_input(input))
+
+
+def intersection(a, b):
+    return [value for value in a if value in b]
 
 
 def star1(data):
     """Solve puzzle for star 1."""
+    prio_dict = build_prio_dict()
+
+    def get_prio(item):
+        return prio_dict[item]
+
+    total_prio = 0
+    for bag in data:
+        comp_one, comp_two = bag
+        common_set = set(intersection(comp_one, comp_two))
+        prio = sum(map(get_prio, common_set))
+        total_prio += prio
+    return total_prio
 
 
 def star2(data):
