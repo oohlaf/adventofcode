e6db87727ceb5b1de456cd90b1aa80ae734e7a2c
Olaf Conradi <olaf@conradi.org>
2022-12-13 23:17:41 +0100

    AoC 2022 Day 8 Star 1

--- 2022/day08/run.py
+++ 2022/day08/run.py
@@ -5,10 +5,52 @@ from pathlib import Path
 
 def parse_data(input):
     """Parse input data."""
+    result = [[int(x) for x in line] for line in input.split("\n")]
+    return result
+
+
+def check_left(x, y, data, w, h):
+    for i in range(0, x):
+        if data[y][i] >= data[y][x]:
+            return 0
+    return 1
+
+
+def check_right(x, y, data, w, h):
+    for i in range(x + 1, w):
+        if data[y][i] >= data[y][x]:
+            return 0
+    return 1
+
+
+def check_top(x, y, data, w, h):
+    for j in range(0, y):
+        if data[j][x] >= data[y][x]:
+            return 0
+    return 1
+
+
+def check_bottom(x, y, data, w, h):
+    for j in range(y + 1, h):
+        if data[j][x] >= data[y][x]:
+            return 0
+    return 1
 
 
 def star1(data):
     """Solve puzzle for star 1."""
+    w = len(data[0])
+    h = len(data)
+    visible = [[1 if (y == 0 or y == h - 1) or (x == 0 or x == w - 1) else 0 for x in range(w)] for y in range(h)]
+    for y in range(1, h - 1):
+        for x in range(1, w - 1):
+            visible[y][x] = (
+                check_left(x, y, data, w, h)
+                or check_right(x, y, data, w, h)
+                or check_top(x, y, data, w, h)
+                or check_bottom(x, y, data, w, h)
+            )
+    return sum(sum(visible, []))
 
 
 def star2(data):
