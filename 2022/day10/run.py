ffca83e1a7ecb64617790c712439bccf5b281000
Olaf Conradi <olaf@conradi.org>
2022-12-17 00:52:15 +0100

    AoC 2022 Day 10 Star 1

--- 2022/day10/run.py
+++ 2022/day10/run.py
@@ -3,12 +3,52 @@
 from pathlib import Path
 
 
+CPU_START_STATE = {"cycle": 1, "X": 1}
+
+
+def ins_noop(state, value):
+    state["cycle"] += 1
+    yield state
+
+
+def ins_addx(state, value):
+    state["cycle"] += 1
+    yield state
+    state["X"] += value
+    state["cycle"] += 1
+    yield state
+
+
+INSTRUCTIONS = {
+    "noop": ins_noop,
+    "addx": ins_addx,
+}
+
+
 def parse_data(input):
     """Parse input data."""
+    result = [(line[0:4], int(line[5:])) if len(line) > 4 else (line[0:4], None) for line in input.split("\n")]
+    return result
+
+
+def print_status(state, ins):
+    print(f"cycle: {state['cycle']}\tX: {state['X']}\tinstruction: {ins[0]} {ins[1]}")
 
 
 def star1(data):
     """Solve puzzle for star 1."""
+    cpu_state = CPU_START_STATE
+    # print_status(cpu_state, (None, None))
+    inspect = [20, 60, 100, 140, 180, 220]
+    signal_strength = 0
+    for ins in data:
+        f = INSTRUCTIONS[ins[0]]
+        for c in f(cpu_state, ins[1]):
+            cpu_state = c
+            if cpu_state['cycle'] in inspect:
+                print_status(cpu_state, ins)
+                signal_strength += cpu_state['cycle'] * cpu_state['X']
+    return signal_strength
 
 
 def star2(data):
