c68632083a3ae1bc99209307a701a2f22a683921
Olaf Conradi <olaf@conradi.org>
2022-12-17 02:07:19 +0100

    AoC 2022 Day 10 Star 2

--- 2022/day10/run.py
+++ 2022/day10/run.py
@@ -3,9 +3,6 @@
 from pathlib import Path
 
 
-CPU_START_STATE = {"cycle": 1, "X": 1}
-
-
 def ins_noop(state, value):
     state["cycle"] += 1
     yield state
@@ -35,9 +32,20 @@ def print_status(state, ins):
     print(f"cycle: {state['cycle']}\tX: {state['X']}\tinstruction: {ins[0]} {ins[1]}")
 
 
+def crt_draw(state):
+    x = (state["cycle"] % 40) - 1
+    # y = state["cycle"] // 40
+    if x == 0:
+        print("\n", end="")
+    if x >= state["X"] - 1 and x <= state["X"] + 1:
+        print("#", end="")
+    else:
+        print(".", end="")
+
+
 def star1(data):
     """Solve puzzle for star 1."""
-    cpu_state = CPU_START_STATE
+    cpu_state = {"cycle": 1, "X": 1}
     # print_status(cpu_state, (None, None))
     inspect = [20, 60, 100, 140, 180, 220]
     signal_strength = 0
@@ -45,14 +53,24 @@ def star1(data):
         f = INSTRUCTIONS[ins[0]]
         for c in f(cpu_state, ins[1]):
             cpu_state = c
-            if cpu_state['cycle'] in inspect:
+            if cpu_state["cycle"] in inspect:
                 print_status(cpu_state, ins)
-                signal_strength += cpu_state['cycle'] * cpu_state['X']
+                signal_strength += cpu_state["cycle"] * cpu_state["X"]
     return signal_strength
 
 
 def star2(data):
     """Solve puzzle for star 2."""
+    cpu_state = {"cycle": 1, "X": 1}
+    # print_status(cpu_state, (None, None))
+    inspect = [20, 60, 100, 140, 180, 220]
+    crt_draw(cpu_state)
+    for ins in data:
+        f = INSTRUCTIONS[ins[0]]
+        for c in f(cpu_state, ins[1]):
+            cpu_state = c
+            crt_draw(cpu_state)
+    print()
 
 
 def solve(input):
